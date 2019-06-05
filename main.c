dc927d07d90d94840e5e0557e7ff4676378a64d6
Hendrik <hendrik.diemer@gmail.com>
2019-06-05 13:18:21 +0200

    Added motherseq generator

--- main.c
+++ main.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+
+int generateMotherSequence(long *sequence, long *indexUsed) {
+    long base[10] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+    for (int i = 0; i < 1023; ++i) {
+
+    }
+
+}
+
+
 int main(int argc, char *argv[]) {
     char *filename = argv[1];
     int i = 0;
@@ -43,6 +54,15 @@ int main(int argc, char *argv[]) {
     for (int j = 0; j < 1023; j++) {
         printf("%ld\n", mySequence[j]);
     }
+
+    long firstMotherSequence[1023];
+    long secondMotherSequence[1023];
+
+    long firstIndices[2] = {-1, 2};
+    long secondIndices[6] = {-1, 1, 2, 5, 7, 8};
+
+    generateMotherSequence(firstMotherSequence, firstIndices);
+    generateMotherSequence(secondMotherSequence, secondIndices);
 
 
     return 0;
